85f39ea02a35b6b5b41a73e269137cedbb36507c
IT-Delinquent <48588045+IT-Delinquent@users.noreply.github.com>
2019-07-17 11:18:13 +0100

    Add project files.

---
+++ WalkingReminder/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WalkingReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Event when the user presses the "Fine..." Button - closes form
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        //Event when the user clicks on the border with the left mouse button - moves form
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
